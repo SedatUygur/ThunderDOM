6960b2b7e9e77fc22cc982c617f741293668e010
Sedat Uygur <sedat.uygur@outlook.com>
2024-12-23 21:04:56 +0300

    ➕ add VirtualNode

--- src/h.js
+++ src/h.js
@@ -1,9 +1,11 @@
+import { VirtualNode } from "./vnode";
+
 // Create an HTML element description
 export const h = (tag, props, children) => {
-  return { tag, props, children };
+  return new VirtualNode({ tag, props, children });
 };
 
 // Create a text element description
 export const text = (content) => {
-  return { text: content };
+  return new VirtualNode({ text: content });
 };
---
+++ src/vnode.js
@@ -0,0 +1,5 @@
+export class VirtualNode {
+  constructor(any) {
+    Object.assign(this, any);
+  }
+}
